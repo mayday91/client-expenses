ea6ff19e52a12f5a8754ad38910f6914d66049f7
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-09-09 15:12:31 -0400

    changed api url for deployment

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: '<replace_with_deployed_api_url>',
+	production: 'https://expenses.fly.dev',
 	development: 'http://localhost:8000',
 }
 
