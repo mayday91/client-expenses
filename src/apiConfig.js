76899a82c1628e62996cf10333bff0e1e92e82d3
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-09-10 17:53:52 -0400

    changed production url for server in apiconfig back to railway

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://mayday-proxy.herokuapp.com/https://mayday-expenses.herokuapp.com/',
+	production: 'https://mayday-proxy.herokuapp.com/https://https://expenses-server.up.railway.app',
 	development: 'http://localhost:3001',
 }
 
