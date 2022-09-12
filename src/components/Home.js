45c46b378246dd39c3b646c440e6a3b58db93b98
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-09-12 13:39:57 -0400

    added enum options for type and category in expenseform

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import IndexExpenses from './expenses/IndexExpenses'
 import MyPieChart from './pie-chart/PieChart'
 import './Home.css'
+import React, { useMediaQuery } from 'react'
+
+
 
 const Home = (props) => {
 	const { msgAlert, user } = props
 	console.log('props in home', msgAlert)
 
+	
 	return (
 		<div className='homeItems'>
 			<div className="pieChart">
--- src/components/expenses/ExpenseForm.js
+++ src/components/expenses/ExpenseForm.js
@@ -26,14 +26,40 @@ const ExpenseForm = (props) => {
           value={ expense.amount}
           onChange={ handleChange }
         />
+         <Form.Label htmlFor="type">Type:</Form.Label>
+        <Form.Select 
+        aria-label="type"
+        name='type'
+        defaultValue={expense.type}
+        onChange={ handleChange }
+      >
+        <option>Income or Expense:</option>
+        <option value='income'>Income</option>
+        <option value='expense'>Expense</option>
+      </Form.Select>
         <Form.Label htmlFor="category">Category:</Form.Label>
-        <Form.Control
-          placeholder="What category is this in?"
-          name="category"
-          id="category"
-          value={ expense.category }
-          onChange={ handleChange }
-        />
+        <Form.Select 
+        aria-label="category"
+        name='category'
+        defaultValue={expense.category}
+        onChange={ handleChange }
+      >
+        <option>Pick a category:</option>
+        <option value='utilities'>Utilities</option>
+        <option value='housing'>Housing</option>
+        <option value='ent'>Entertainment</option>
+        <option value='food'>Food</option>
+        <option value='travel'>Travel</option>
+        <option value='savings'>Savings</option>
+        <option value='investment'>Investment</option>
+        <option value='loan'>Loan</option>
+        <option value='personal'>Personal</option>
+        <option value='healthcare'>Healthcare</option>
+        <option value='debt'>Debt</option>
+        <option value='credit'>Credit</option>
+        <option value='tech'>Tech</option>
+        <option value='misc.'>Miscellaneous</option>
+      </Form.Select>
         <Button type="submit">Submit</Button>
       </Form>
     </Container>
