73e7251a2caaac73f56b55aa6ea0db7375bdd973
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-11-22 17:27:42 -0500

    changed proxy in apiconfig

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://expenses.fly.dev',
+	production: 'https://polar-brook-07094.herokuapp.com/https://expenses.fly.dev',
 	development: 'http://localhost:3001',
 }
 
